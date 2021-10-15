fc405920a0f41577a3c492eb76860915cebc73f4
Kevin Mezu <42013409+uzomezu@users.noreply.github.com>
2021-10-15 13:06:10 -0400

    strapi backend installation

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c0223100f142c918fe463e6fb6cc06ad'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4c271e9c-c7dd-42d4-8c95-aa823b030888'
+};
